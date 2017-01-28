433824a6dd1bd0210d2c130c1adcb32cbb5927e1
PetrNikolas <elprokop7@gmail.com>
2017-01-28 18:15:22 +0100

    https://github.com/PetrNikolas/Rails-Starter-Kit/issues/3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.paginate(page: params[:page], per_page: 10)
   end
 
   # GET /posts/1
