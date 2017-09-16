5b4867c4629eade41430689e01ead80cf1456d1b
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2017-09-16 15:50:04 +0200

    update and versioning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
-
+  # Devise for authentication
   devise_for :users
+
+  # Resources
   resources :posts
 
-  get 'home/index'
-
+  # Root of application
   root :to => "home#index"
+  get '/home' => 'home#index'
 
-  get '/api' => 'news#index', defaults: { format: :json }
+  # API endpoints
+  scope '/api' do
+    # Version 1
+    scope '/v1' do
+      get '/news' => 'news#index', defaults: { format: :json }
+    end
+  end
 
 end
