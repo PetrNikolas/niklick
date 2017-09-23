514ecfd8789b0774343d0c4b4ddac89513d24ce9
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2017-09-23 13:36:58 +0200

    change whitelist to safelist

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,7 @@
 class Rack::Attack 
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new 
 
-  whitelist('allow-localhost') do |req| 
+  safelist('allow-localhost') do |req| 
     '127.0.0.1' == req.ip || '::1' == req.ip 
   end
 
