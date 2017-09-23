b1eb657889dc0d9628d8795daf583e2b512d1cea
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2017-09-23 11:29:24 +0200

    comment

--- app/controllers/v1/news_controller.rb
+++ app/controllers/v1/news_controller.rb
@@ -1,5 +1,7 @@
 module V1
    class NewsController < ApplicationController
+        # GET /news
+        # GET /news.json
         def index 
             respond_to do |format| 
                 format.json { render json: {some: 'data'} } 
