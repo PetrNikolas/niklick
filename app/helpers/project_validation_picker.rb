2687803044b538de28fbe045c18d7ab9f508bc37
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2017-12-03 12:03:28 +0100

    add comment

--- app/helpers/project_validation_picker.rb
+++ app/helpers/project_validation_picker.rb
@@ -1,4 +1,5 @@
 class ProjectValidationPicker
+    # Pick validation on API endpoints
     def pick_a_validation(project, api=false)
         if api
            project.instance_variable_set(:@strict_priority_validation, false)
