cfb884d76c8371a00423521ced32af1207af30f3
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2017-12-30 18:37:46 +0100

    remove devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({
+  uid: 'johnreed',
+  username: 'johnreed',
+  provider: 'email',
+  name: 'John Reed',
+  email: 'johnreed@johnreed.com',
+  password: '12345678',
+  password_confirmation: '12345678'
+})
