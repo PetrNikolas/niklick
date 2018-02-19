af876a0bd46b274d102282a49cc7b1d55f2cc8ce
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-02-19 20:04:44 +0100

    updates

--- app/controllers/v1/graphql_controller.rb
+++ app/controllers/v1/graphql_controller.rb
@@ -1,21 +1,17 @@
 module V1
   class GraphqlController < BaseApiController
     before_action :authenticate_user!
-    before_action :set_current_user
 
     def execute
       query = params[:query]
       query_variables = JSON.load(params[:variables]) || {}
-      context = { current_user: @user }
+      context = {
+        current_user: current_user
+      }
 
       result = Schema.execute(query, variables: query_variables, context: context)
 
       render json: result
     end
-
-    private
-      def set_current_user
-        @user = current_user
-      end
   end
 end
