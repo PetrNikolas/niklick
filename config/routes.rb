efe37dd5f8fbc2a77852ad26e77f6c8209fdcefe
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2018-12-02 16:30:05 +0100

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,13 @@ Rails.application.routes.draw do
   get 'privacy', to: 'pages#privacy'
   get 'terms', to: 'pages#terms'
 
+  # GraphiQL playground in browser - for just only development env
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/api/v1/graphql' if Rails.env.development?
 
   # API endpoints
-  scope '/api', module: :api, constraints: AuthConstraint.new(token: 'OllBS57MeAW041dgR7xolpZaiO87kjnMVVK3qrtgtTbcBolB7K3TugZBuM6') do
+  scope '/api', module: :api, defaults: { format: :json }, constraints: AuthConstraint.new(token: 'OllBS57MeAW041dgR7xolpZaiO87kjnMVVK3qrtgtTbcBolB7K3TugZBuM6') do
     # Version 1 of API
-    scope '/v1', module: :v1, defaults: { format: :json } do
+    scope '/v1', module: :v1 do
       # Endpoint for GraphQL endpoints
       post '/graphql' => 'graphql#execute'
 
