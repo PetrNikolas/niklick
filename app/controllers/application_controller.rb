a1e0dcc6c549f90a13b94f4ec6d9865ef598f0c0
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2019-01-26 21:42:14 +0100

    add devise config to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   respond_to :json
   protect_from_forgery with: :null_session
+
+  protected
+
+  # Add custom fields to the Devise auth
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: %i[nickname name]
+    devise_parameter_sanitizer.permit :account_update, keys: %i[nickname name]
+  end
 end
