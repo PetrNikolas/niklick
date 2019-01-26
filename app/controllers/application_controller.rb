6dc5aeaaf390c0fd54cfe5a3f5e1111011055594
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2019-01-26 19:30:31 +0100

    remove collect_metrics method from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::Base
-  around_action :collect_metrics
   respond_to :json
 
   protect_from_forgery with: :null_session
-
-  # Request metrics for each controller
-  def collect_metrics
-    start = Time.now
-    yield
-    duration = Time.now - start
-    Rails.logger.info "Controller #{controller_name} and #{action_name} action - duration #{duration}s"
-  end
 end
