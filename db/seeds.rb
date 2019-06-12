f12239bbb5410c49d37049f50942ad09118c8693
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2019-06-12 19:34:05 +0200

    update cache control and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  News.create full_name: Faker::StarWars.character,
+                  email: Faker::Internet.unique.email,
+                  gender: (rand > 0.5 ? 'male' : 'female'),
+                  salary: Faker::Number.between(1, 5000)
+
+
+    t.string   "title"
+    t.string   "description"
+    t.string   "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_news_on_user_id", using: :btree
+end
