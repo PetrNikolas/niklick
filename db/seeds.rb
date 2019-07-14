0bf5c729a1a7bd33298c9b50458007b3c3d286f4
Petr Nikolas Prokop <petrnikolasprokop@yahoo.com>
2019-07-14 16:38:00 +0200

    fix: faker require in db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 10.times do
   News.create full_name: Faker::StarWars.character,
                   email: Faker::Internet.unique.email,
